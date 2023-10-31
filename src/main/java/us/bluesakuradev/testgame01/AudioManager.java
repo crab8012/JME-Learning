3cd0e0f8164c62dc64b555a7893a255c9ec6cba5
Jordan Petersen <crab8011@gmail.com>
2023-10-31 05:29:49 +0000

    Adding Audio and Input Tests

---
+++ src/main/java/us/bluesakuradev/testgame01/AudioManager.java
@@ -0,0 +1,48 @@
+package us.bluesakuradev.testgame01;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.audio.AudioData;
+import com.jme3.audio.AudioNode;
+
+import java.util.HashMap;
+
+public class AudioManager {
+    HashMap<String, AudioNode> sfx;
+    HashMap<String, AudioNode> mus;
+    AssetManager assetManager;
+
+    public AudioManager(AssetManager am){
+        sfx = new HashMap<>();
+        mus = new HashMap<>();
+
+        this.assetManager = am;
+    }
+
+    public void init(){
+        // Initialise all the audio
+        sfx.put("error", new AudioNode(assetManager, "Sounds/error1.wav", AudioData.DataType.Buffer));
+        sfx.put("good", new AudioNode(assetManager, "Sounds/good1.wav", AudioData.DataType.Buffer));
+        sfx.put("jump", new AudioNode(assetManager, "Sounds/jump1.wav", AudioData.DataType.Buffer));
+        sfx.put("laser1", new AudioNode(assetManager, "Sounds/laser1.wav", AudioData.DataType.Buffer));
+        sfx.put("laser2", new AudioNode(assetManager, "Sounds/laser2.wav", AudioData.DataType.Buffer));
+        sfx.put("notif", new AudioNode(assetManager, "Sounds/notif1.wav", AudioData.DataType.Buffer));
+        sfx.put("ping1", new AudioNode(assetManager, "Sounds/ping1.wav", AudioData.DataType.Buffer));
+        sfx.put("ping2", new AudioNode(assetManager, "Sounds/ping2.wav", AudioData.DataType.Buffer));
+        sfx.put("thud", new AudioNode(assetManager, "Sounds/thud1.wav", AudioData.DataType.Buffer));
+    }
+
+    public void addSfx(String keyName, String audioName){
+        sfx.put(keyName, new AudioNode(assetManager, "Sounds/"+audioName, AudioData.DataType.Buffer));
+    }
+
+    public void addMusic(String keyName, String audioName){
+        mus.put(keyName, new AudioNode(assetManager, "Music/"+audioName, AudioData.DataType.Stream));
+    }
+
+    public AudioNode getSfx(String keyName){
+        return sfx.get(keyName);
+    }
+    public AudioNode getMusic(String keyName){
+        return mus.get(keyName);
+    }
+}
--- src/main/java/us/bluesakuradev/testgame01/Main.java
+++ src/main/java/us/bluesakuradev/testgame01/Main.java
@@ -1,7 +1,15 @@
 package us.bluesakuradev.testgame01;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.ScreenshotAppState;
+import com.jme3.app.state.VideoRecorderAppState;
+import com.jme3.audio.AudioData;
+import com.jme3.audio.AudioNode;
+import com.jme3.audio.Environment;
 import com.jme3.font.BitmapText;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
@@ -11,6 +19,7 @@ import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.system.AppSettings;
+import com.jme3.util.BufferUtils;
 import com.jme3.util.SkyFactory;
 
 import java.awt.*;
@@ -25,25 +34,139 @@ public class Main extends SimpleApplication {
     private float boxMoveStep = 0.05f;
     BitmapText boxPos;
     BitmapText camPos;
+    BitmapText audioInfo;
+
+    AudioManager am;
+    boolean reverb = false;
+    String audioFx = "NONE";
+
+    final VideoRecorderAppState recState = new VideoRecorderAppState();
+
+    final private ActionListener customActionListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean pressed, float tpf) {
+            if(name.equals("sound1") && pressed){
+                am.getSfx("ping1").setReverbEnabled(reverb);
+                am.getSfx("ping1").play();
+            }
+            if(name.equals("sound2") && pressed){
+                am.getSfx("error").setReverbEnabled(reverb);
+                am.getSfx("error").play();
+            }
+            if(name.equals("sound3") && pressed){
+                am.getSfx("notif").setReverbEnabled(reverb);
+                am.getSfx("notif").play();
+            }
+            if(name.equals("sound4") && pressed){
+                am.getSfx("thud").setReverbEnabled(reverb);
+                am.getSfx("thud").play();
+            }
+            if(name.equals("mus1") && pressed){
+                AudioNode mus = am.getMusic("commaxian");
+                mus.setReverbEnabled(reverb);
+                mus.setPositional(false);
+                am.getMusic("commaxian").play();
+            }
+            if(name.equals("musStop") && pressed){
+                am.getMusic("commaxian").stop();
+            }
+            if(name.equals("reverb") && pressed){
+                reverb = !reverb;
+                System.out.println("Reverb: " + reverb);
+            }
+        }
+    };
+
+    final private ActionListener audioEnvActionListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean pressed, float tpf) {
+            if(name.equals("cavern") && pressed){
+                audioRenderer.setEnvironment(new Environment(Environment.Cavern));
+                audioFx = "Cavern";
+            }
+            if(name.equals("garage") && pressed){
+                audioRenderer.setEnvironment(new Environment(Environment.Garage));
+                audioFx = "Garage";
+            }
+            if(name.equals("lab") && pressed){
+                audioRenderer.setEnvironment(new Environment(Environment.AcousticLab));
+                audioFx = "Lab";
+            }
+            if(name.equals("closet") && pressed){
+                audioRenderer.setEnvironment(new Environment(Environment.Closet));
+                audioFx = "Closet";
+            }
+            if(name.equals("dungeon") && pressed){
+                audioRenderer.setEnvironment(new Environment(Environment.Dungeon));
+                audioFx = "Dungeon";
+            }
+        }
+    };
+
+    final private ActionListener stateListener = new ActionListener() {
+        @Override
+        public void onAction(String name, boolean pressed, float tpf) {
+            if(name.equals("startRec") && pressed){
+                stateManager.attach(recState);
+                System.out.println("Screen Recording Started");
+            }
+            if(name.equals("stopRec") && pressed){
+                stateManager.detach(recState);
+                System.out.println("Screen Recording Stopped");
+            }
+        }
+    };
 
     public static void main(String[] args){
         Main app = new Main();
         AppSettings settings = new AppSettings(true);
         settings.setTitle("Test Game 01");
+        settings.setResolution(1024,768);
         app.setSettings(settings);
-
+        BufferUtils.setTrackDirectMemoryEnabled(true);
         app.start();
     }
     @Override
     public void simpleInitApp() {
+        ScreenshotAppState screenShotState = new ScreenshotAppState();
+        this.stateManager.attach(screenShotState);
         initSceneGeometry();
         initSceneUI();
+        setKeyMapping();
+
+        // Audio
+        am = new AudioManager(assetManager);
+        am.init();
+
+        // Add commaxian song
+        am.addMusic("commaxian", "commaxian.ogg");
 
         // Sky
         //rootNode.attachChild(SkyFactory.createSky(getAssetManager(), "Textures/Sky/BrightSky.dds", SkyFactory.EnvMapType.CubeMap));
         rootNode.attachChild(SkyFactory.createSky(getAssetManager(), "Textures/Sky/PureSky.jpg", SkyFactory.EnvMapType.SphereMap));
         rootNode.attachChild(geom);
         rootNode.attachChild(geom2);
+    }
+    private void setKeyMapping(){
+        inputManager.addListener(customActionListener, new String[]{"sound1", "sound2", "sound3", "sound4", "mus1", "musStop", "reverb"});
+        inputManager.addMapping("sound1", new KeyTrigger(KeyInput.KEY_1));
+        inputManager.addMapping("sound2", new KeyTrigger(KeyInput.KEY_2));
+        inputManager.addMapping("sound3", new KeyTrigger(KeyInput.KEY_3));
+        inputManager.addMapping("sound4", new KeyTrigger(KeyInput.KEY_4));
+        inputManager.addMapping("mus1", new KeyTrigger(KeyInput.KEY_PGUP));
+        inputManager.addMapping("musStop", new KeyTrigger(KeyInput.KEY_PGDN));
+
+        inputManager.addListener(audioEnvActionListener, new String[]{"cavern", "garage", "lab", "closet", "dungeon"});
+        inputManager.addMapping("cavern", new KeyTrigger(KeyInput.KEY_0));
+        inputManager.addMapping("garage", new KeyTrigger(KeyInput.KEY_9));
+        inputManager.addMapping("lab", new KeyTrigger(KeyInput.KEY_8));
+        inputManager.addMapping("closet", new KeyTrigger(KeyInput.KEY_7));
+        inputManager.addMapping("dungeon", new KeyTrigger(KeyInput.KEY_6));
+        inputManager.addMapping("reverb", new KeyTrigger(KeyInput.KEY_BACK));
+
+        inputManager.addListener(stateListener, new String[]{"startRec", "stopRec"});
+        inputManager.addMapping("startRec", new KeyTrigger(KeyInput.KEY_J));
+        inputManager.addMapping("stopRec", new KeyTrigger(KeyInput.KEY_K));
     }
 
     private void initSceneGeometry(){
@@ -84,6 +207,13 @@ public class Main extends SimpleApplication {
         camPos.setText("Camera Position");
         camPos.setLocalTranslation(20, camPos.getLineHeight()*40,0);
         gui.attachChild(camPos);
+
+        audioInfo = new BitmapText(guiFont, false);
+        audioInfo.setSize(guiFont.getCharSet().getRenderedSize());
+        audioInfo.setColor(ColorRGBA.White);
+        audioInfo.setText("Audio Information");
+        audioInfo.setLocalTranslation(this.getCamera().getWidth()-audioInfo.getLineWidth(), this.getCamera().getHeight()-audioInfo.getLineHeight(),0);
+        gui.attachChild(audioInfo);
     }
 
     private float randomSmallFloat(){
@@ -123,5 +253,8 @@ public class Main extends SimpleApplication {
         String camPosOut = "Cam Position: [%fx - %fy - %fz] | Cam Rotation: [%fx - %fy - %fz - %fw]";
 
         camPos.setText(String.format(camPosOut, cloc.getX(), cloc.getY(), cloc.getZ(), crot.getX(), crot.getY(), crot.getZ(), crot.getW()));
+
+        audioInfo.setText("Reverb: " + reverb + " Audio Effect: " + audioFx);
+        audioInfo.setLocalTranslation(this.getCamera().getWidth()-audioInfo.getLineWidth(), this.getCamera().getHeight(),0);
     }
 }
